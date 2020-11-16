e6c2094ed3c57db54300087fa3ba255c3158a301
akthrms <akt.hrms@gmail.com>
2020-11-17 01:46:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# 動的計画法（Dynamic Programming, DP）
+
+TODO
+
+## 動的計画法
+
+TODO
+
+## ベンチマーク
+
+[benchmark.js](https://benchmarkjs.com/)
+
+### フィボナッチ数列
+
+```
+フィボナッチ数列 * 10【DPなし】 x 999,663 ops/sec ±1.70% (94 runs sampled)
+フィボナッチ数列 * 10【メモ化】 x 1,616,030 ops/sec ±0.39% (94 runs sampled)
+フィボナッチ数列 * 10【DPあり】 x 7,623,889 ops/sec ±1.08% (88 runs sampled)
+フィボナッチ数列 * 30【DPなし】 x 69.29 ops/sec ±0.24% (70 runs sampled)
+フィボナッチ数列 * 30【メモ化】 x 585,690 ops/sec ±0.41% (94 runs sampled)
+フィボナッチ数列 * 30【DPあり】 x 3,418,598 ops/sec ±0.55% (93 runs sampled)
+```
---
+++ src/fib.js
@@ -0,0 +1,84 @@
+const Benchmark = require("benchmark");
+
+/**
+ * フィボナッチ数列
+ * @param {number} n
+ */
+function fib(n) {
+  if (n == 0) {
+    return 0;
+  } else if (n == 1) {
+    return 1;
+  } else {
+    return fib(n - 2) + fib(n - 1);
+  }
+}
+
+/**
+ * フィボナッチ数列（メモ化）
+ * @param {number} n
+ */
+function fib_memo(n) {
+  const memo = {};
+
+  function _fib_memo(m) {
+    if (m in memo) {
+      return memo[m];
+    } else {
+      let result;
+
+      if (m == 0) {
+        result = 0;
+      } else if (m == 1) {
+        result = 1;
+      } else {
+        result = _fib_memo(m - 2) + _fib_memo(m - 1);
+      }
+
+      memo[m] = result;
+      return result;
+    }
+  }
+
+  return _fib_memo(n);
+}
+
+/**
+ * フィボナッチ数列（DP）
+ * @param {number} n
+ */
+function fib_dp(n) {
+  const dp = [...Array(n)];
+  dp[0] = 0;
+  dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 2] + dp[i - 1];
+  }
+
+  return dp[n];
+}
+
+new Benchmark.Suite()
+  .add("フィボナッチ数列 * 10【DPなし】", () => {
+    fib(10);
+  })
+  .add("フィボナッチ数列 * 10【メモ化】", () => {
+    fib_memo(10);
+  })
+  .add("フィボナッチ数列 * 10【DPあり】", () => {
+    fib_dp(10);
+  })
+  .add("フィボナッチ数列 * 30【DPなし】", () => {
+    fib(30);
+  })
+  .add("フィボナッチ数列 * 30【メモ化】", () => {
+    fib_memo(30);
+  })
+  .add("フィボナッチ数列 * 30【DPあり】", () => {
+    fib_dp(30);
+  })
+  .on("cycle", (event) => {
+    console.log(String(event.target));
+  })
+  .run();
