0c75f339b375f077eecdab309829276a383f82b0
akthrms <akt.hrms@gmail.com>
2020-11-18 20:42:00 +0900

    update

--- src/a__flog_1.js
+++ src/a__flog_1.js
@@ -62,23 +62,30 @@ function a__frog_1_memo(xs) {
  * @param {Array<number>} xs
  */
 function a__frog_1_dp(xs) {
-  const n = xs.length - 1;
-  const dp = Array(n);
+  const len = xs.length;
+  const dp = Array(len).fill(Math.pow(10, 4));
   dp[0] = 0;
+  dp[1] = Math.abs(xs[1] - xs[0]);
 
-  for (let i = 1; i <= n; i++) {
-    if (i == 1) {
-      dp[i] = Math.abs(xs[i] - xs[i - 1]);
-    } else {
-      dp[i] = Math.min(
-        dp[i - 1] + Math.abs(xs[i] - xs[i - 1]),
-        dp[i - 2] + Math.abs(xs[i] - xs[i - 2])
-      );
-    }
+  for (let i = 2; i <= len - 1; i++) {
+    dp[i] = Math.min(
+      dp[i - 1] + Math.abs(xs[i] - xs[i - 1]),
+      dp[i - 2] + Math.abs(xs[i] - xs[i - 2])
+    );
   }
 
-  return dp[n];
+  return dp[len - 1];
 }
+
+console.assert(30 == a__frog_1([10, 30, 40, 20]));
+console.assert(30 == a__frog_1_memo([10, 30, 40, 20]));
+console.assert(30 == a__frog_1_dp([10, 30, 40, 20]));
+console.assert(0 == a__frog_1([10, 10]));
+console.assert(0 == a__frog_1_memo([10, 10]));
+console.assert(0 == a__frog_1_dp([10, 10]));
+console.assert(40 == a__frog_1([30, 10, 60, 10, 60, 50]));
+console.assert(40 == a__frog_1_memo([30, 10, 60, 10, 60, 50]));
+console.assert(40 == a__frog_1_dp([30, 10, 60, 10, 60, 50]));
 
 new Benchmark.Suite()
   .add("A - Frog 1 [10, 30, 40, 20]【DPなし】", () => {
